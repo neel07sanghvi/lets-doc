04df0149b7be7bcd80841bd596478924bfe4a0c9
neelsanghvi <neel07sanghvi@gmail.com>
2025-09-09 14:41:40 +0530

    Updates

---
+++ _next/static/chunks/3bc7c564fb81fb62.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/37e735bacc74b58c.js",
+  "static/chunks/turbopack-276beaf8c89f5f96.js"
+])
---
+++ _next/static/chunks/fc346532daa7187f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/37e735bacc74b58c.js",
+  "static/chunks/turbopack-754ec162c870adf0.js"
+])
